4254d2abc71c6f43f51a31063bb2cb84e67e7a4b
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-15 23:18:07 +0300

    Updated api-router.js

--- api-router.js
+++ api-router.js
@@ -1,8 +1,14 @@
 const router= require("express").Router();
 const categoryController=require("./controllers/categoryController")
+const bookController =require("./controllers/bookController")
 
 // http://localhost/api/category
 router.route("/category").get(categoryController.list).post(categoryController.create)
 
+// http://localhost/api/category/131232136
 router.route("/category/:category_id").put(categoryController.update).delete(categoryController.delete)
+
+router.route("/book").get(bookController.list);
+
+
 module.exports=router
