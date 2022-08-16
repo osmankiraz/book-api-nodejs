e4fa138f879f36776b37008a1ffbf34908541473
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-16 23:04:23 +0300

    Updated api-router.js

--- api-router.js
+++ api-router.js
@@ -6,9 +6,11 @@ const bookController =require("./controllers/bookController")
 router.route("/category").get(categoryController.list).post(categoryController.create)
 
 // http://localhost/api/category/131232136
-router.route("/category/:category_id").put(categoryController.update).delete(categoryController.delete)
+router.route("/category/:category_id").put(categoryController.update).delete(categoryController.delete).get(categoryController.getById)
 
-router.route("/book").get(bookController.list);
+router.route("/book").get(bookController.list).post(bookController.create)
+router.route("/book/:book_id").get(bookController.getById).put(bookController.update).delete(bookController.delete)
 
+router.route("/books/:category_id").get(bookController.listByCategoryId)
 
 module.exports=router
