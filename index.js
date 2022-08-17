1b8f1aca9178d821ac4ae380d82dbb9f73c89928
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-17 23:21:45 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ let bodyParser = require("body-parser");
 require('./db/connection')
 const dotenv = require("dotenv").config();
 let cors = require("cors");
+
+
 const apiRouter=require("./api-router")
-
-
-
 let port = process.env.port;
-
 
 let app = new express();
 app.use(cors());
+
+app.use(express.static("assets/images"))
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
