c5abde66dbc16c75fbe0639e73ff4b773a8de750
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-17 23:22:07 +0300

    Updated bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,22 @@
 const Book = require("../models/book.model");
 const response = require("../response");
+const { validationResult } = require("express-validator");
+const multer = require("multer");
+const path = require("path");
+let bookFileName = null;
+
+let myStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./assets/images");
+  },
+  filename: function (req, file, cb) {
+    bookFileName = Date.now() + path.extname(file.originalname);
+    cb(null, bookFileName);
+  },
+});
+exports.upload = multer({
+  storage: myStorage,
+});
 
 exports.list = (req, res) => {
   Book.find({})
@@ -20,19 +37,23 @@ exports.getById = (req, res) => {
     });
 };
 
+exports.listByCategoryId = (req, res) => {
+  const _id = req.params.category_id;
 
-exports.listByCategoryId=(req,res)=>{
-  const _id=req.params.category_id
-
-  Book.find({categoryBy:_id}).populate("categoryBy").exec((err,books)=>{
-    if(err) return new response(null,err).error500(res);
-    return new response(books,null).success(res);
-  })
-
-}
-
+  Book.find({ categoryBy: _id })
+    .populate("categoryBy")
+    .exec((err, books) => {
+      if (err) return new response(null, err).error500(res);
+      return new response(books, null).success(res);
+    });
+};
 
 exports.create = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return new response(null, errors.array()).badRequest(res);
+  }
+
   const { title, author, price, stock, picture, categoryBy } = req.body;
   let book = new Book();
   book.title = title;
@@ -49,6 +70,11 @@ exports.create = (req, res) => {
 };
 
 exports.update = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return new response(null, errors.array()).badRequest(res);
+  }
+
   Book.findById(req.params.book_id, (err, book) => {
     if (err) return new response(null, err).error500(res);
     if (!book) return new response().notFound(res);
@@ -61,21 +87,32 @@ exports.update = (req, res) => {
     book.picture = picture;
     book.categoryBy = categoryBy._id;
 
-    book.save((err)=>{
-      if(err) return new response(null,err).error500(res);
-      return new response(book,null).success(res)
-    })
+    book.save((err) => {
+      if (err) return new response(null, err).error500(res);
+      return new response(book, null).success(res);
+    });
   });
 };
 
+exports.delete = (req, res) => {
+  let _id = req.params.book_id;
 
-exports.delete=(req,res)=>{
-  let _id=req.params.book_id
+  Book.findOneAndDelete({ _id: _id }, (err, book) => {
+    if (err) return new response(null, err).error500(res);
+    if (!book) return new response().notFound(res);
+    return new response(book, null).success(res);
+  });
+};
 
-  Book.findOneAndDelete({_id:_id},(err,book)=>{
-    if(err) return new response(null,err).error500(res)
-    if(!book) return new response().notFound(res);
-    return new response(book,null).success(res);
-
-  })
-}
+exports.saveImage = (req, res) => {
+  try {
+    res
+      .status(200)
+      .json({
+        status: "success",
+        url: `http://localhost:${process.env.port}/${bookFileName}`,
+      });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+};
